c207bb6c216863d0249621bb347dd37c87560f3f
Delucse <46593742+Delucse@users.noreply.github.com>
2019-11-13 11:52:45 +0100

    Picture Upload with Multer

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
+// jshint esversion: 8
+// jshint node: true
+"use strict";
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 
 var app = express();
+
+
+function connectMongoDB() {
+  (async () => {
+    // set up default ("Docker") mongoose connection
+    await mongoose.connect('mongodb://localhost:27017/openBadges', {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      autoReconnect: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    }).then(db => {
+      console.log('Connected to MongoDB (databasename: "'+db.connections[0].name+'") on host "'+db.connections[0].host+'" and on port "'+db.connections[0].port+'""');
+    }).catch(err => {
+        console.log('Error at MongoDB-connection: '+err);
+        console.log('Retry to connect in 3 seconds');
+        setTimeout(connectMongoDB, 3000); // retry until db-server is up
+      });
+  })();
+}
+// connect to MongoDB
+connectMongoDB();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +46,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var pictureRouter = require('./routes/picture');
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/picture', pictureRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
