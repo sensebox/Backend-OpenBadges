001cd0a52691521c84207397cd0f2aaaab25cadc
hilal98 <38653823+hilal98@users.noreply.github.com>
2019-11-13 11:52:10 +0100

    First steps

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Open Badges' });
 });
 
+/* GET Start page. */
+router.get('/Start', function(req, res, next) {
+  res.render('startseite');
+});
 module.exports = router;
