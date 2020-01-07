36476b89550c1d4c980b5602a41c2f2eafed046d
Leg-en <44808950+Leg-en@users.noreply.github.com>
2020-01-07 11:07:39 +0100

    Startsite erstellt,Layout angepasst,Kursregistrierung angepasst,index erweitertm Sofwtwarekurs erstellt

--- routes/index.js
+++ routes/index.js
@@ -38,4 +38,7 @@ router.get('/pwlost', function(req, res, next) {
 router.get('/badgelist', function(req, res, next) {
   res.render('badgelist');
 });
+router.get('/Softwarekurs', function(req, res, next) {
+  res.render('Softwarekurs');
+});
 module.exports = router;
